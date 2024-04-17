017813c85a28c3e992db03fc403518d7d28ed145
Connor Johnson <34697277+wendulem@users.noreply.github.com>
2024-04-17 14:54:02 -0400

    Basic skeleton for CreateProduct w no API usage

---
+++ src/components/CreateProduct.js
@@ -0,0 +1,13 @@
+// src/components/CreateProduct.js
+import React from 'react';
+
+const CreateProduct = () => {
+  return (
+    <div>
+      <h1>Create Your Product</h1>
+      {/* Include form to submit new product details */}
+    </div>
+  );
+};
+
+export default CreateProduct;
